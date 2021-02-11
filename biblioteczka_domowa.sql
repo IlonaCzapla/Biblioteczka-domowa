b0c5bb979ef1e785e0b552a2ce0f58115d61c658
IlonaCzapla <ilona.krzak@gmail.com>
2021-02-11 19:44:08 +0100

    poprawa znaków

--- biblioteczka_domowa.sql
+++ biblioteczka_domowa.sql
@@ -49,7 +49,7 @@ rodzaj_okladki varchar(10),
 id_ksiazki int foreign key (id_ksiazki) references Ksiazki(id_ksiazki) ,
 );
 
-/*  tworzymy tabele ³¹czac¹ Gatunek z Ksiazkami */
+/*  tworzymy tabele Å‚Ä…czacÄ… Gatunek z Ksiazkami */
 
 create table Gatunek_ksiazki
 (id_gatunku int not null,
@@ -59,7 +59,7 @@ constraint FK_Gatunek_ksiazki_id_ksiazki Foreign key (id_ksiazki) references Ksi
 constraint FK_Gatunek_ksiazki_id_gatunku Foreign key (id_gatunku) references Gatunek (id_gatunku)
 );
 
-/* tworzymy tabele ³¹czaca tabele Ksiazki i Autor */
+/* tworzymy tabele Å‚Ä…czaca tabele Ksiazki i Autor */
 
 create table Autorzy_ksiazki
 (id_autora int not null,
@@ -72,9 +72,9 @@ constraint FK_Autorzy_ksiazki_id_ksiazki foreign key (id_ksiazki) references Ksi
 /* wstawianie danych do tabeli Gatunek */
 
 insert into Gatunek (nazwa_gatunku) values
-('powieœæ dla dzieci'),
+('powieÅ›Ä‡ dla dzieci'),
 ('opowiadanie'),
-('podró¿nicze'),
+('podrÃ³Å¼nicze'),
 ('edukacyjne'),
 ('z okienkami'),
 ('wierszowane'),
@@ -94,7 +94,7 @@ insert into Wydawnictwo (nazwa_wydawnictwa) values
 ('Olesiejuk'),
 ('Zakamarki'),
 ('Zielona Sowa'),
-('Nasza Ksiêgarnia'),
+('Nasza KsiÄ™garnia'),
 ('Dwie siostry')
 
 
@@ -102,28 +102,28 @@ insert into Wydawnictwo (nazwa_wydawnictwa) values
 
 insert into Ksiazki (tytul, rok_wydania, id_wydawnictwa, isbn, seria)
 values
-('Koala, który siê trzyma³', 2016,8, '978380735927', null),
-('Wiewiórki, które nie chcia³y siê dzieliæ', 2017, 8, '9788380737259', null),
-('Mysz, która chcia³a byæ lewem', 2015, 8,'9788380735934', null ),
-('Wilk, który siê zgubi³', 2018,8,'9788380739482', null), 
+('Koala, ktÃ³ry siÄ™ trzymaÅ‚', 2016,8, '978380735927', null),
+('WiewiÃ³rki, ktÃ³re nie chciaÅ‚y siÄ™ dzieliÄ‡', 2017, 8, '9788380737259', null),
+('Mysz, ktÃ³ra chciaÅ‚a byÄ‡ lewem', 2015, 8,'9788380735934', null ),
+('Wilk, ktÃ³ry siÄ™ zgubiÅ‚', 2018,8,'9788380739482', null), 
 ('Trzymaj mnie mocno', 2019,8,'9788380737587', null),
-('Noc na ulicy Czereœniowej', 2013,10, '9788363696405', 'Czereœniowa'),
-('Wiosna na ulicy Czereœniowej',2012,10, '9788365341167', 'Czereœniowa'), 
-('Zima na ulicy Czereœniowej', 2012,10, '9788365341150', 'Czereœniowa'),
-('Jesieñ na ulicy Czereœniowej', 2012,10, '9788365341181', 'Czereœniowa'),
-('Lato na ulicy Czereœniowej', 2012,10, '9788365341174', 'Czereœniowa'),
+('Noc na ulicy CzereÅ›niowej', 2013,10, '9788363696405', 'CzereÅ›niowa'),
+('Wiosna na ulicy CzereÅ›niowej',2012,10, '9788365341167', 'CzereÅ›niowa'), 
+('Zima na ulicy CzereÅ›niowej', 2012,10, '9788365341150', 'CzereÅ›niowa'),
+('JesieÅ„ na ulicy CzereÅ›niowej', 2012,10, '9788365341181', 'CzereÅ›niowa'),
+('Lato na ulicy CzereÅ›niowej', 2012,10, '9788365341174', 'CzereÅ›niowa'),
 ('Tola na wsi', 2019, 8, '9788381541510', 'Tola'),
 ('Tola w przedszkolu', 2019,8, '9788381543774', 'Tola'),
 ('Zima Toli', 2018,8, '9788380739246', 'Tola'),
 ('Lato Toli', 2019,8, '9788380738119', 'Tola'),
 ('Wiosna Toli', 2018,8, '9788380738102', 'Tola'),
-('Jesieñ Toli', 2018,8, '9788380739239', 'Tola'),
-('Jadzia Pêtelka zostaje z niani¹', 2020, 8, '9788381546898', 'Jadzia Pêtelka'),
-('Jadzia Pêtelka idzie spaæ', 2019,8, '9788381543224', 'Jadzia Pêtelka'),
-('Jadzia Pêtelka nie odda ³opatki', 2020,8, '9788381545839', 'Jadzia Pêtelka'),
-('Jadzia Pêtelka robi siku', 2019, 8, '9788381543866', 'Jadzia Pêtelka'),
-('Jadzia Ptelka idzie na spacer', 2020,8, '9788381545358', 'Jadzia Pêtelka'),
-('Jadzia Pêtelka piecze pierniki', 2020,8, '9788381547734', 'Jadzia Pêtelka');
+('JesieÅ„ Toli', 2018,8, '9788380739239', 'Tola'),
+('Jadzia PÄ™telka zostaje z nianiÄ…', 2020, 8, '9788381546898', 'Jadzia PÄ™telka'),
+('Jadzia PÄ™telka idzie spaÄ‡', 2019,8, '9788381543224', 'Jadzia PÄ™telka'),
+('Jadzia PÄ™telka nie odda Å‚opatki', 2020,8, '9788381545839', 'Jadzia PÄ™telka'),
+('Jadzia PÄ™telka robi siku', 2019, 8, '9788381543866', 'Jadzia PÄ™telka'),
+('Jadzia PÄ™telka idzie na spacer', 2020,8, '9788381545358', 'Jadzia PÄ™telka'),
+('Jadzia PÄ™telka piecze pierniki', 2020,8, '9788381547734', 'Jadzia PÄ™telka');
 
 /* select * from Ksiazki*/
 
@@ -133,11 +133,11 @@ insert into Autor (imie_autora, nazwisko_autora) values
 ('Rachel',  'Bright'),
 ('Jim', 'Field'),
 ('Rotraut Susanne', 'Berner'),
-('Anna', 'W³odarkiewicz'),
-('Barbara', 'Supe³'),
+('Anna', 'WÅ‚odarkiewicz'),
+('Barbara', 'SupeÅ‚'),
 ('Jane', 'Chapman');
 
-/*po³aczenie ksiazek z autorami*/
+/*poÅ‚aczenie ksiazek z autorami*/
 insert into Autorzy_ksiazki (id_ksiazki, id_autora) values
 (1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(4,2),(5,6),(6,3),
 (7,3),(8,3),(9,3),(10,3),(11, 4),(12,4),(13,4),(14,4),(15,4),
@@ -145,7 +145,7 @@ insert into Autorzy_ksiazki (id_ksiazki, id_autora) values
 
 --select * from Autorzy_ksiazki;
 
-/* dopisanie gatunkow do ksi¹zek */
+/* dopisanie gatunkow do ksiÄ…zek */
 
 insert into Gatunek_ksiazki (id_ksiazki, id_gatunku) values
 (1,6), (2,6), (3,6), (4,6), (5,2), (6,7), (7,7), (8,7), (9,7), (10,7),
@@ -155,7 +155,7 @@ insert into Gatunek_ksiazki (id_ksiazki, id_gatunku) values
 
 
 /* tworzenie procedury spDodajKsiazke, 
-ktora dodaje ksiazke, badz informuje, ¿e taka ksi¹zka juz jest w bazie */
+ktora dodaje ksiazke, badz informuje, Å¼e taka ksiÄ…zka juz jest w bazie */
 
 GO
 create or alter procedure spDodajKsiazke
@@ -169,7 +169,7 @@ as
 begin
 	if exists (select * from Ksiazki where isbn=@isbn)
 	begin
-	raiserror ('Jest ju¿ taka ksi¹¿ka w bazie!', 11,1 )
+	raiserror ('Jest juÅ¼ taka ksiÄ…Å¼ka w bazie!', 11,1 )
 	return -1
 	end
 	
@@ -179,7 +179,7 @@ begin
 end;
 
 
-/*tworzenie procedury spDodaj Autora, która sprawdza, czy taki autor ju¿ jest w bazie i zwraca id autora */
+/*tworzenie procedury spDodaj Autora, ktÃ³ra sprawdza, czy taki autor juÅ¼ jest w bazie i zwraca id autora */
 
 go
 create or alter procedure spDodajAutora
@@ -190,7 +190,7 @@ as
 begin
 	if exists (Select * from Autor where imie_autora=@imie_autora and nazwisko_autora=@nazwisko_autora)
 	begin
-	print 'Jest ju¿ taki autor w bazie'
+	print 'Jest juÅ¼ taki autor w bazie'
 	select @id_autora= id_autora from Autor where imie_autora=@imie_autora and nazwisko_autora=@nazwisko_autora
 	end
 	else
@@ -203,7 +203,7 @@ begin
 end;
 
 
-/* dodanie procedury ³aczacej autora i ksiazke*/
+/* dodanie procedury Å‚aczacej autora i ksiazke*/
 go
 create or alter procedure spPolaczAutoraZKsiazka
 @id_ksiazki int,
@@ -222,14 +222,14 @@ as
 begin
 	if exists (select * from Gatunek where nazwa_gatunku=@nazwa_gatunku)
 		begin
-		raiserror ('Jest ju¿ taki gatunek',11,1)
+		raiserror ('Jest juÅ¼ taki gatunek',11,1)
 		end
 	else
 	insert into Gatunek (nazwa_gatunku) values (@nazwa_gatunku)
 	select @id_gatunku= SCOPE_IDENTITY()
 end;
 
-/* dodanie procedury ³acz¹cej Gatunek z Ksiazka*/
+/* dodanie procedury Å‚aczÄ…cej Gatunek z Ksiazka*/
 go
 create procedure spPolaczGatunekZKsiazka
 @id_gatunku int,
@@ -249,7 +249,7 @@ as
 begin
 	if exists (select * from Wydawnictwo where nazwa_wydawnictwa=@nazwa_wydawnictwa)
 		begin
-		raiserror ('Jest ju¿ takie wydawnictwo',11,1)
+		raiserror ('Jest juÅ¼ takie wydawnictwo',11,1)
 		end
 	else
 	insert into Wydawnictwo (nazwa_wydawnictwa) values (@nazwa_wydawnictwa)
@@ -259,15 +259,15 @@ end;
 go
 
 
-/*  wywo³anie procedury spDodaj ksiazke */
+/*  wywoÅ‚anie procedury spDodaj ksiazke */
 
 declare @id_k int;
 exec spDodajKsiazke @isbn='9788310129079', 
-@tytul='Z mucha na luzie æwiczymy buzie, czyli zabawy logopedyczne dla dzieci',  @rok_wydania=2015, @id_ksiazki=@id_k output, 
+@tytul='Z mucha na luzie Ä‡wiczymy buzie, czyli zabawy logopedyczne dla dzieci',  @rok_wydania=2015, @id_ksiazki=@id_k output, 
 @podtytul= null, @id_wydawnictwa=8;
 select @id_k as id_ksiazki;
 
-/* wywo³anie procedury spDodajAutora */
+/* wywoÅ‚anie procedury spDodajAutora */
 
 declare @id_a int;
 exec spDodajAutora @imie_autora='Marta', @nazwisko_autora='Galewska-Kustra', @id_autora=@id_a output;
@@ -276,33 +276,33 @@ select @id_a as id_autora;
 select * from Autorzy_ksiazki
 
 
-/* wywo³anie procedury ³¹cz¹cej Autora z Ksi¹zka */
+/* wywoÅ‚anie procedury Å‚Ä…czÄ…cej Autora z KsiÄ…zka */
 exec spPolaczAutoraZKsiazka @id_ksiazki=23, @id_autora=7;
 select * from Autorzy_ksiazki
 
-/*wywo³anie procedury spDodajGatunek */
+/*wywoÅ‚anie procedury spDodajGatunek */
 
 declare @id_gat int
-exec spDodajGatunek @nazwa_gatunku='anglojêzyczne', @id_gatunku=@id_gat output
+exec spDodajGatunek @nazwa_gatunku='anglojÄ™zyczne', @id_gatunku=@id_gat output
 select @id_gat as id_gatunku;
 
-/* wywo³anie procedury ³acz¹cej Gatunek z ksia¿ka */
+/* wywoÅ‚anie procedury Å‚aczÄ…cej Gatunek z ksiaÄ…ka */
 
 exec spPolaczGatunekZKsiazka @id_gatunku=7, @id_ksiazki=23
 
-/* wywo³anie procedury spDodajWydawnictwo */
+/* wywoÅ‚anie procedury spDodajWydawnictwo */
 
 declare @id_wyd int
-exec  spDodajWydawnictwo @nazwa_wydawnictwa='Nasza Ksiêgarnia1', @id_wydawnictwa=@id_wyd output;
+exec  spDodajWydawnictwo @nazwa_wydawnictwa='Nasza KsiÄ™garnia', @id_wydawnictwa=@id_wyd output;
 select @id_wyd as id_wydawnictwa;
 
 
-/*--wyœwielenie  podstawowych danych o ksi¹¿kach */
+/*--wyÅ›wielenie  podstawowych danych o ksiÄ…Å¼kach */
 
 select k.id_ksiazki
 , k.tytul
 , k.rok_wydania
-, a.imie_autora + ' '+a.nazwisko_autora as 'Imiê_Nazwisko_Autora'
+, a.imie_autora + ' '+a.nazwisko_autora as 'ImiÄ™_Nazwisko_Autora'
 , g.nazwa_gatunku
 , w.nazwa_wydawnictwa
 from Ksiazki k
