b2c82aec3afbbb2f408496a750710e663d103ea8
IlonaCzapla <ilona.krzak@gmail.com>
2021-03-12 21:05:49 +0100

    upper

--- biblioteczka_domowa.sql
+++ biblioteczka_domowa.sql
@@ -1,77 +1,79 @@
-drop database Biblioteczka_domowa;
-create database Biblioteczka_domowa;
+DROP DATABASE  Biblioteczka_domowa;
+CREATE DATABASE Biblioteczka_domowa;
 
-go
-use Biblioteczka_domowa;
-go
+GO
+USE Biblioteczka_domowa;
+GO
 /*   tworzymy tabele Gatunek   */
 
-create table Gatunek
-(id_gatunku int IDENTITY primary key ,
-nazwa_gatunku varchar(50) unique
+CREATE TABLE Gatunek
+(id_gatunku INT IDENTITY PRIMARY KEY ,
+nazwa_gatunku VARCHAR(50) UNIQUE 
 );
 
 /*  tworzymy tabele Wydawnictwo  */
-
-create table Wydawnictwo
+GO
+CREATE TABLE Wydawnictwo
 (
-id_wydawnictwa int IDENTITY primary key,
-nazwa_wydawnictwa varchar(50) unique
+id_wydawnictwa INT IDENTITY PRIMARY KEY ,
+nazwa_wydawnictwa VARCHAR(50) UNIQUE
 );
 
 /*  tworzymy tabele Autor  */
-
-create table Autor
+GO
+CREATE TABLE Autor
 (
-Id_autora int not null identity primary key,
-imie_autora varchar(20) not null,
-nazwisko_autora varchar (50) not null
+Id_autora INT NOT NULL IDENTITY PRIMARY KEY,
+imie_autora VARCHAR(20) NOT NULL,
+nazwisko_autora VARCHAR (50) NOT NULL,
+CONSTRAINT UQ_AUTOR_IMIE_NAZWISKO UNIQUE (imie_autora, nazwisko_autora)
+
 );
 
 /*  tworzymy tabele Ksiazki  */
-
-create table Ksiazki
+GO
+CREATE TABLE Ksiazki
 ( 
-id_ksiazki int not null identity primary key,
-tytul varchar (100) not null unique,
-podtytul varchar(200),
-seria varchar(200),
-rok_wydania int,
-id_wydawnictwa int foreign key (id_wydawnictwa) references Wydawnictwo (id_wydawnictwa),
-isbn varchar(13) Unique
+id_ksiazki INT NOT NULL IDENTITY PRIMARY KEY ,
+tytul VARCHAR (100) NOT NULL,
+podtytul VARCHAR (200),
+seria VARCHAR (200),
+rok_wydania INT ,
+id_wydawnictwa INT FOREIGN KEY (id_wydawnictwa) REFERENCES Wydawnictwo (id_wydawnictwa),
+isbn VARCHAR(13) UNIQUE
 );
 
 /*  tworzymy tabele Opis fizyczny  */
-
-create table Opis_fizyczny
-(format_ksiazki varchar(2),
-rodzaj_okladki varchar(10),
-id_ksiazki int foreign key (id_ksiazki) references Ksiazki(id_ksiazki) ,
+GO
+CREATE TABLE Opis_fizyczny
+(format_ksiazki VARCHAR(2),
+rodzaj_okladki VARCHAR(10),
+id_ksiazki INT FOREIGN KEY (id_ksiazki) REFERENCES Ksiazki(id_ksiazki) ,
 );
 
 /*  tworzymy tabele łączacą Gatunek z Ksiazkami */
-
-create table Gatunek_ksiazki
-(id_gatunku int not null,
-id_ksiazki int not null,
-constraint PK_Gatunek_ksiazki Primary key (id_ksiazki, id_gatunku),
-constraint FK_Gatunek_ksiazki_id_ksiazki Foreign key (id_ksiazki) references Ksiazki(id_ksiazki),
-constraint FK_Gatunek_ksiazki_id_gatunku Foreign key (id_gatunku) references Gatunek (id_gatunku)
+GO
+CREATE TABLE Gatunek_ksiazki
+(id_gatunku INT NOT NULL,
+id_ksiazki INT NOT NULL ,
+CONSTRAINT PK_Gatunek_ksiazki PRIMARY KEY (id_ksiazki, id_gatunku),
+CONSTRAINT FK_Gatunek_ksiazki_id_ksiazki FOREIGN KEY  (id_ksiazki) REFERENCES Ksiazki(id_ksiazki),
+CONSTRAINT FK_Gatunek_ksiazki_id_gatunku FOREIGN KEY (id_gatunku) REFERENCES Gatunek (id_gatunku)
 );
 
 /* tworzymy tabele łączaca tabele Ksiazki i Autor */
-
-create table Autorzy_ksiazki
-(id_autora int not null,
-id_ksiazki int not null,
-constraint PK_Autorzy_ksiazki primary key (id_ksiazki, id_autora),
-constraint FK_Autorzy_ksiazki_id_autora foreign key (id_autora) references Autor(id_autora),
-constraint FK_Autorzy_ksiazki_id_ksiazki foreign key (id_ksiazki) references Ksiazki(id_ksiazki),
+GO
+CREATE TABLE Autorzy_ksiazki
+(id_autora INT NOT NULL,
+id_ksiazki INT NOT NULL,
+CONSTRAINT PK_Autorzy_ksiazki PRIMARY KEY (id_ksiazki, id_autora),
+CONSTRAINT FK_Autorzy_ksiazki_id_autora FOREIGN KEY (id_autora) REFERENCES Autor(id_autora),
+CONSTRAINT FK_Autorzy_ksiazki_id_ksiazki FOREIGN KEY (id_ksiazki) REFERENCES Ksiazki(id_ksiazki),
 );
 
 /* wstawianie danych do tabeli Gatunek */
-
-insert into Gatunek (nazwa_gatunku) values
+GO
+INSERT INTO Gatunek (nazwa_gatunku) VALUES
 ('powieść dla dzieci'),
 ('opowiadanie'),
 ('podróżnicze'),
@@ -84,8 +86,8 @@ insert into Gatunek (nazwa_gatunku) values
 
 
 /* wstawianie danych do tabeli Wydawnictwo */
-
-insert into Wydawnictwo (nazwa_wydawnictwa) values
+GO
+INSERT INTO Wydawnictwo (nazwa_wydawnictwa) VALUES
 ('Egmont'),
 ('Babaryba'),
 ('Wigla'),
@@ -95,18 +97,18 @@ insert into Wydawnictwo (nazwa_wydawnictwa) values
 ('Zakamarki'),
 ('Zielona Sowa'),
 ('Nasza Księgarnia'),
-('Dwie siostry')
+('Dwie siostry');
 
 
 /* wstawiane danych do tabeli Ksiazki */
-
-insert into Ksiazki (tytul, rok_wydania, id_wydawnictwa, isbn, seria)
-values
-('Koala, który się trzymał', 2016,8, '978380735927', null),
-('Wiewiórki, które nie chciały się dzielić', 2017, 8, '9788380737259', null),
-('Mysz, która chciała być lewem', 2015, 8,'9788380735934', null ),
-('Wilk, który się zgubił', 2018,8,'9788380739482', null), 
-('Trzymaj mnie mocno', 2019,8,'9788380737587', null),
+GO
+INSERT INTO Ksiazki (tytul, rok_wydania, id_wydawnictwa, isbn, seria)
+VALUES
+('Koala, który się trzymał', 2016,8, '978380735927', NULL),
+('Wiewiórki, które nie chciały się dzielić', 2017, 8, '9788380737259', NULL),
+('Mysz, która chciała być lewem', 2015, 8,'9788380735934', NULL ),
+('Wilk, który się zgubił', 2018,8,'9788380739482', NULL), 
+('Trzymaj mnie mocno', 2019,8,'9788380737587', NULL),
 ('Noc na ulicy Czereśniowej', 2013,10, '9788363696405', 'Czereśniowa'),
 ('Wiosna na ulicy Czereśniowej',2012,10, '9788365341167', 'Czereśniowa'), 
 ('Zima na ulicy Czereśniowej', 2012,10, '9788365341150', 'Czereśniowa'),
@@ -128,8 +130,8 @@ values
 /* select * from Ksiazki*/
 
 /* wstawienie wierszy do tabeli Autor */
-
-insert into Autor (imie_autora, nazwisko_autora) values
+GO
+INSERT INTO Autor (imie_autora, nazwisko_autora) VALUES
 ('Rachel',  'Bright'),
 ('Jim', 'Field'),
 ('Rotraut Susanne', 'Berner'),
@@ -138,7 +140,8 @@ insert into Autor (imie_autora, nazwisko_autora) values
 ('Jane', 'Chapman');
 
 /*połaczenie ksiazek z autorami*/
-insert into Autorzy_ksiazki (id_ksiazki, id_autora) values
+GO
+INSERT INTO Autorzy_ksiazki (id_ksiazki, id_autora) VALUES
 (1,1),(1,2),(2,1),(2,2),(3,1),(3,2),(4,1),(4,2),(5,6),(6,3),
 (7,3),(8,3),(9,3),(10,3),(11, 4),(12,4),(13,4),(14,4),(15,4),
 (16,4), (17,5), (18,5), (19,5), (20,5), (21,5), (22,5);
@@ -146,8 +149,8 @@ insert into Autorzy_ksiazki (id_ksiazki, id_autora) values
 --select * from Autorzy_ksiazki;
 
 /* dopisanie gatunkow do ksiązek */
-
-insert into Gatunek_ksiazki (id_ksiazki, id_gatunku) values
+GO
+INSERT INTO Gatunek_ksiazki (id_ksiazki, id_gatunku) VALUES
 (1,6), (2,6), (3,6), (4,6), (5,2), (6,7), (7,7), (8,7), (9,7), (10,7),
 (11,2), (12,2), (13,2), (14,2), (15,2), (16,2), (17, 2), (18,2), (19,2),
 (20,2), (21,2), (22,2);
@@ -158,163 +161,165 @@ insert into Gatunek_ksiazki (id_ksiazki, id_gatunku) values
 ktora dodaje ksiazke, badz informuje, że taka ksiązka juz jest w bazie */
 
 GO
-create or alter procedure spDodajKsiazke
-@isbn varchar(13),
-@tytul varchar(100),
-@podtytul varchar(200),
-@rok_wydania int,
-@id_wydawnictwa int,
-@id_ksiazki int output
-as
-begin
-	if exists (select * from Ksiazki where isbn=@isbn)
-	begin
-	raiserror ('Jest już taka książka w bazie!', 11,1 )
-	return -1
-	end
+CREATE OR ALTER PROCEDURE spDodajKsiazke
+@isbn VARCHAR(13),
+@tytul VARCHAR(100),
+@podtytul VARCHAR(200),
+@rok_wydania INT,
+@id_wydawnictwa INT,
+@id_ksiazki INT OUTPUT
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Ksiazki WHERE isbn=@isbn)
+	BEGIN
+	RAISERROR ('Jest już taka książka w bazie!', 11,1 )
+	RETURN -1
+	END
 	
-	insert into dbo.Ksiazki (isbn, tytul, podtytul, rok_wydania, id_wydawnictwa) 
-	values (@isbn, @tytul, @podtytul, @rok_wydania, @id_wydawnictwa)
-	select  @id_ksiazki =scope_identity ()
-end;
+	INSERT INTO Ksiazki (isbn, tytul, podtytul, rok_wydania, id_wydawnictwa) 
+	VALUES (@isbn, @tytul, @podtytul, @rok_wydania, @id_wydawnictwa)
+	SELECT  @id_ksiazki =scope_identity ()
+END ;
 
 
 /*tworzenie procedury spDodaj Autora, która sprawdza, czy taki autor już jest w bazie i zwraca id autora */
 
-go
-create or alter procedure spDodajAutora
-@imie_autora Varchar(20),
-@nazwisko_autora varchar(50),
-@id_autora int output
-as
-begin
-	if exists (Select * from Autor where imie_autora=@imie_autora and nazwisko_autora=@nazwisko_autora)
-	begin
-	print 'Jest już taki autor w bazie'
-	select @id_autora= id_autora from Autor where imie_autora=@imie_autora and nazwisko_autora=@nazwisko_autora
-	end
-	else
-	begin
-	print 'Nie ma takiego autora w bazie'
-	insert into Autor (imie_autora, nazwisko_autora)
-	values (@imie_autora, @nazwisko_autora)
-	select @id_autora = SCOPE_IDENTITY()
-	end
-end;
+GO
+CREATE OR ALTER PROCEDURE spDodajAutora
+@imie_autora VARCHAR(20),
+@nazwisko_autora VARCHAR(50),
+@id_autora INT OUTPUT
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Autor WHERE imie_autora=@imie_autora AND nazwisko_autora=@nazwisko_autora)
+	BEGIN
+	PRINT 'Jest już taki autor w bazie'
+	SELECT @id_autora= id_autora FROM Autor WHERE imie_autora=@imie_autora AND nazwisko_autora=@nazwisko_autora
+	END
+	ELSE
+	BEGIN
+	PRINT 'Nie ma takiego autora w bazie'
+	INSERT INTO Autor (imie_autora, nazwisko_autora)
+	VALUES (@imie_autora, @nazwisko_autora)
+	SELECT @id_autora = SCOPE_IDENTITY()
+	END
+END;
 
 
 /* dodanie procedury łaczacej autora i ksiazke*/
-go
-create or alter procedure spPolaczAutoraZKsiazka
-@id_ksiazki int,
-@id_autora int
-as
-begin
-insert into Autorzy_ksiazki (id_ksiazki, id_autora) values (@id_ksiazki, @id_autora)
-end;
+GO
+CREATE OR ALTER PROCEDURE spPolaczAutoraZKsiazka
+@id_ksiazki INT,
+@id_autora INT
+AS
+BEGIN
+INSERT INTO Autorzy_ksiazki (id_ksiazki, id_autora) VALUES (@id_ksiazki, @id_autora)
+END;
 
 /* dodanie procedury do dodania Gatunku */
-go
-create or alter procedure spDodajGatunek
-@nazwa_gatunku varchar(50),
-@id_gatunku int output
-as
-begin
-	if exists (select * from Gatunek where nazwa_gatunku=@nazwa_gatunku)
-		begin
-		raiserror ('Jest już taki gatunek',11,1)
-		end
-	else
-	insert into Gatunek (nazwa_gatunku) values (@nazwa_gatunku)
-	select @id_gatunku= SCOPE_IDENTITY()
-end;
+GO
+CREATE OR ALTER PROCEDURE spDodajGatunek
+@nazwa_gatunku VARCHAR (50),
+@id_gatunku INT OUTPUT
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Gatunek WHERE nazwa_gatunku=@nazwa_gatunku)
+		BEGIN
+		RAISERROR ('Jest już taki gatunek',11,1)
+		END
+	ELSE
+	INSERT INTO Gatunek (nazwa_gatunku) VALUES (@nazwa_gatunku)
+	SELECT @id_gatunku= SCOPE_IDENTITY()
+END;
 
 /* dodanie procedury łaczącej Gatunek z Ksiazka*/
-go
-create procedure spPolaczGatunekZKsiazka
-@id_gatunku int,
-@id_ksiazki int
-as
-begin
-insert into Gatunek_ksiazki(id_gatunku, id_ksiazki) values (@id_gatunku, @id_ksiazki)
-end;
+GO
+CREATE PROCEDURE spPolaczGatunekZKsiazka
+@id_gatunku INT,
+@id_ksiazki INT
+AS
+BEGIN
+INSERT INTO Gatunek_ksiazki(id_gatunku, id_ksiazki) VALUES (@id_gatunku, @id_ksiazki)
+END;
 
 
 /*dodanie procedury dodajacej wydawnictwo*/
-go
-create or alter procedure spDodajWydawnictwo
-@nazwa_wydawnictwa varchar(50),
-@id_wydawnictwa int output
-as
-begin
-	if exists (select * from Wydawnictwo where nazwa_wydawnictwa=@nazwa_wydawnictwa)
-		begin
-		raiserror ('Jest już takie wydawnictwo',11,1)
-		end
-	else
-	insert into Wydawnictwo (nazwa_wydawnictwa) values (@nazwa_wydawnictwa)
-	select @id_wydawnictwa= SCOPE_IDENTITY()
-end;
+GO
+CREATE OR ALTER PROCEDURE spDodajWydawnictwo
+@nazwa_wydawnictwa VARCHAR(50),
+@id_wydawnictwa INT OUTPUT
+AS
+BEGIN
+	IF EXISTS (SELECT * FROM Wydawnictwo WHERE nazwa_wydawnictwa=@nazwa_wydawnictwa)
+		BEGIN
+		RAISERROR ('Jest już takie wydawnictwo',11,1)
+		END
+	ELSE
+	INSERT INTO Wydawnictwo (nazwa_wydawnictwa) VALUES (@nazwa_wydawnictwa)
+	SELECT @id_wydawnictwa= SCOPE_IDENTITY()
+END;
 
-go
+
 
 
 /*  wywołanie procedury spDodaj ksiazke */
-
-declare @id_k int;
-exec spDodajKsiazke @isbn='9788310129079', 
-@tytul='Z mucha na luzie ćwiczymy buzie, czyli zabawy logopedyczne dla dzieci',  @rok_wydania=2015, @id_ksiazki=@id_k output, 
-@podtytul= null, @id_wydawnictwa=8;
-select @id_k as id_ksiazki;
+GO
+DECLARE @id_k INT;
+EXEC spDodajKsiazke @isbn='9788310129079', 
+@tytul='Z mucha na luzie ćwiczymy buzie, czyli zabawy logopedyczne dla dzieci',  @rok_wydania=2015, @id_ksiazki=@id_k OUTPUT, 
+@podtytul= NULL , @id_wydawnictwa=8;
+SELECT @id_k AS id_ksiazki;
 
 /* wywołanie procedury spDodajAutora */
-
-declare @id_a int;
-exec spDodajAutora @imie_autora='Marta', @nazwisko_autora='Galewska-Kustra', @id_autora=@id_a output;
-select @id_a as id_autora;
-
+GO
+DECLARE @id_a INT;
+EXEC spDodajAutora @imie_autora='Marta', @nazwisko_autora='Galewska-Kustra', @id_autora=@id_a OUTPUT;
+SELECT @id_a AS id_autora;
+GO
 select * from Autorzy_ksiazki
 
 
 /* wywołanie procedury łączącej Autora z Ksiązka */
-exec spPolaczAutoraZKsiazka @id_ksiazki=23, @id_autora=7;
-select * from Autorzy_ksiazki
+GO
+EXEC spPolaczAutoraZKsiazka @id_ksiazki=23, @id_autora=7;
+SELECT * FROM Autorzy_ksiazki
 
 /*wywołanie procedury spDodajGatunek */
-
-declare @id_gat int
-exec spDodajGatunek @nazwa_gatunku='anglojęzyczne', @id_gatunku=@id_gat output
-select @id_gat as id_gatunku;
+GO
+DECLARE @id_gat INT
+EXEC spDodajGatunek @nazwa_gatunku='anglojęzyczne', @id_gatunku=@id_gat OUTPUT
+SELECT @id_gat AS id_gatunku;
 
 /* wywołanie procedury łaczącej Gatunek z książka */
-
-exec spPolaczGatunekZKsiazka @id_gatunku=7, @id_ksiazki=23
+GO
+EXEC spPolaczGatunekZKsiazka @id_gatunku=7, @id_ksiazki=23
 
 /* wywołanie procedury spDodajWydawnictwo */
-
-declare @id_wyd int
-exec  spDodajWydawnictwo @nazwa_wydawnictwa='Nasza Księgarnia', @id_wydawnictwa=@id_wyd output;
-select @id_wyd as id_wydawnictwa;
+GO
+DECLARE @id_wyd INT
+EXEC  spDodajWydawnictwo @nazwa_wydawnictwa='Nasza Księgarnia', @id_wydawnictwa=@id_wyd OUTPUT;
+SELECT @id_wyd AS id_wydawnictwa;
 
 
 /*--wyświelenie  podstawowych danych o książkach */
 GO
 CREATE VIEW PodstawoweDaneKsiazki AS
-select k.id_ksiazki
+SELECT k.id_ksiazki
 , k.tytul
+, k.isbn
 , k.rok_wydania
-, a.imie_autora + ' '+a.nazwisko_autora as 'Imię_Nazwisko_Autora'
+, a.imie_autora + ' '+a.nazwisko_autora AS 'Imię_Nazwisko_Autora'
 , g.nazwa_gatunku
 , w.nazwa_wydawnictwa
-from Ksiazki k
-left join Autorzy_ksiazki ak on ak.id_ksiazki=k.id_ksiazki
-left join Autor a on a.Id_autora=ak.id_autora
-left join Gatunek_ksiazki gk on gk.id_ksiazki=k.id_ksiazki
-left join Gatunek g on g.id_gatunku=gk.id_gatunku
-left join Wydawnictwo w on w.id_wydawnictwa=k.id_wydawnictwa
+FROM Ksiazki k
+LEFT JOIN Autorzy_ksiazki ak ON ak.id_ksiazki=k.id_ksiazki
+LEFT JOIN Autor a ON a.Id_autora=ak.id_autora
+LEFT JOIN Gatunek_ksiazki gk ON gk.id_ksiazki=k.id_ksiazki
+LEFT JOIN Gatunek g ON g.id_gatunku=gk.id_gatunku
+LEFT JOIN Wydawnictwo w ON w.id_wydawnictwa=k.id_wydawnictwa
 
 GO
 SELECT * FROM PodstawoweDaneKsiazki
 GO
 
-
+select * from Ksiazki
